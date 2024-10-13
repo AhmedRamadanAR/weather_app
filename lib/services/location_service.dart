01b99e59c4aefe75483156badf5ca77f12126bfa
mo2mohame <momen7607@gmail.com>
2024-10-13 09:37:55 +0300

    Add GPS location tracker feature

--- lib/services/location_service.dart
+++ lib/services/location_service.dart
@@ -2,32 +2,49 @@ import 'package:geolocator/geolocator.dart';
 import 'package:geocoding/geocoding.dart';
 
 class LocationService {
+
+  // Check if location services are enabled and permissions are granted
   Future<Position?> determinePosition() async {
     bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
     if (!serviceEnabled) {
-      return null;
+      print("Location services are disabled.");
+      return null; // Or return a specific error message
     }
 
     LocationPermission permission = await Geolocator.checkPermission();
     if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
       if (permission == LocationPermission.denied) {
+        print("Location permissions are denied.");
         return null;
       }
     }
 
     if (permission == LocationPermission.deniedForever) {
+      print("Location permissions are permanently denied.");
       return null;
     }
 
-    return await Geolocator.getCurrentPosition();
+    try {
+      // Get the current position of the user
+      return await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    } catch (e) {
+      print("Error getting location: $e");
+      return null;
+    }
   }
 
+  // Get the city name from the latitude and longitude using reverse geocoding
   Future<String?> getCityName(double latitude, double longitude) async {
     try {
       List<Placemark> placemarks = await placemarkFromCoordinates(latitude, longitude);
-      Placemark place = placemarks[0];
-      return place.locality;
+      if (placemarks.isNotEmpty) {
+        Placemark place = placemarks[0];
+        return place.locality; // Return the city name
+      } else {
+        print("No placemarks found for the given coordinates.");
+        return null;
+      }
     } catch (e) {
       print("Error retrieving city name: $e");
       return null;
